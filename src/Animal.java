c2d036a2eb8c384a1efd48d806bff917f376e039
Juan Diego <quintanajuanchy25@gmail.com>
2024-11-01 16:27:36 -0300

    practice poo with person, cat and dog

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    String nombre;
+    int edad;
+
+    public Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String hacerSonido() {
+        return "Hacer sonido genérico";
+    }
+}
